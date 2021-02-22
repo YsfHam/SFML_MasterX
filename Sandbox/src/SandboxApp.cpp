f4c23d1ace788b8de5cf1495a05c3674ba5b8141
YsfHam <ysf.hamdouni@gmail.com>
2021-02-22 20:38:49 +0100

    Create a TestLayer class

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,18 +1,50 @@
-#include <Main.hpp>
+#include <sfmxEngine/Core/Main.hpp>
 #include <SFML_MasterX.hpp>
 
+
+class TestLayer : public masterX::Layer
+{
+public:
+    void onAttach() override
+    {
+        MASTER_INFO("TestLayer starts {0}", ++s_counter);
+        instanceNum = s_counter;
+    }
+
+    void onDetach() override
+    {
+        MASTER_INFO("TestLayer detached {0}", s_counter--);
+    }
+
+    bool onEvent(sf::Event& event) override
+    {
+        if (event.type == sf::Event::KeyPressed)
+        {
+            MASTER_TRACE("The instance {0} received the event", instanceNum);
+
+            return true;
+        }
+        return false;
+    }
+private:
+    static int s_counter;
+    int instanceNum;
+};
+
+int TestLayer::s_counter = 0;
 
 class SandBoxApp : public masterX::Application
 {
 public:
     SandBoxApp()
     {
+        pushLayer(new TestLayer);
     }
 
     void initWindowProps(masterX::WindowProps& props) override
     {
-        props.width = 800;
-        props.height = 700;
+        props.width = 400;
+        props.height = 300;
         props.title = "Hello SFML";
     }
 
